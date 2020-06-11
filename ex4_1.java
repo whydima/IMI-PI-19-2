21be7eccb8492fe6c2e237ac7bd5944d79590e57
whydima <dmitrii.iakovliev.01@mail.ru>
2020-06-11 20:38:16 +0900

    первый пример

---
+++ ex4_1.java
@@ -0,0 +1,12 @@
+public class ex4_1 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new RuntimeException("Непроверяемая ошибка");
+
+        } catch (RuntimeException e) {
+            System.out.println("1 "+ e);
+        }
+        System.out.println("2");
+    }
+}
