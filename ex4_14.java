e07614c13cb0771e540739c973cdebf0d4574de0
whydima <dmitrii.iakovliev.01@mail.ru>
2020-06-11 20:41:07 +0900

    четырнадцатый пример

---
+++ ex4_14.java
@@ -0,0 +1,14 @@
+public class ex4_14 {
+    public static void m(int x) throws ArithmeticException{
+        int h=10/x;
+    }
+    public static void main(String[] args) {
+        try {
+            int l = args.length;
+            System.out.println("размер массива= " + l);
+            m(l);
+        } catch (ArithmeticException e) {
+            System.out.println("Ошибка: Деление на 0");
+        }
+    }
+}
