0b3b87bcad54f90c9826a72aed7c6800b23fcbc5
whydima <dmitrii.iakovliev.01@mail.ru>
2020-06-11 20:39:39 +0900

    восьмой пример

---
+++ ex4_8.java
@@ -0,0 +1,12 @@
+public class ex4_8 {
+    public static int m(){
+    try {
+        System.out.println("0");
+        throw new RuntimeException();
+    } finally {
+        System.out.println("1");
+    } }
+    public static void main(String[] args) {
+        System.out.println(m());
+    }
+}
