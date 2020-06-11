f9e39685354c4fbd3fccde80ac417b633fc67eaf
whydima <dmitrii.iakovliev.01@mail.ru>
2020-06-11 20:39:21 +0900

    шестой пример

---
+++ ex4_6.java
@@ -0,0 +1,16 @@
+public class ex4_6 {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new NullPointerException("ошибка");
+        } catch (ArithmeticException e) {
+            System.out.println("1" );
+        }catch (RuntimeException e) {
+            System.out.println("3" );
+        }catch (Exception e) {
+            System.out.println("2" );
+        }
+
+        System.out.println("4");
+    }
+}
